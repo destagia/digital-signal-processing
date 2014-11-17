4dc9f6322afa0e334c4842711949abdca233bc18
Miyashita Shohei <kei@kamasu.jp>
2014-11-17 15:03:58 +0900

    fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,22 +5,25 @@ import scala.math._
 object Main {
 
 	def main (args:Array[String]) {
-		println("Hello World")
+
 		val sc = new SoundCreater(8000,0.5,6000,500)
-		sc.makeSound("hoge3.raw", (A, f0, samp_freq, n) => {
+
+		val list1 = sc.makeSound((A, f0, samp_freq, n) => {
 			A * sin(2.0 * Pi * f0 * n / samp_freq)
 		})
 
-		sc.makeSound("kukei.raw", (A, f0, samp_freq, n) => {
-			if (n.toInt%10 < 5) {
+		sc.makeFile("sin.raw", list1)
+
+		sc.makeSound((A, f0, samp_freq, n) => {
+			val t0 = 1.0 / f0
+			if (0 <= n && n < (t0 * samp_freq)/2.0) {
 				A
-			} else {
+			} else if ((t0 * samp_freq)/2.0 <= n && n < t0 * samp_freq) {
 				-A
+			} else {
+				0
 			}
 		})
 
 	}
-
-
-
 }
--- src/main/scala/SoundCreater.scala
+++ src/main/scala/SoundCreater.scala
@@ -13,37 +13,39 @@ class SoundCreater(
 
 	val listLength:Double = time * samp_freq
 
-	def makeSound(name:String, function:(Double,Double,Double,Double) => Double) {
-		// Short型をそのままバイナリに書き込んでも，C言語と同じバイナリにならなかった。
-		// おそらく，unsigedとかが絡んでるけど，よくわからんからごり押し。
-		def fetch (s:Short) = {
-			val strTemp = "%04x".format(s)
-			val str = 
-				if (strTemp.length() != 4) {
-					strTemp.replace("ffff", "")
-				} else {
-					strTemp
-				}
+	// Short型をそのままバイナリに書き込んでも，C言語と同じバイナリにならなかった。
+	// おそらく，unsigedとかが絡んでるけど，よくわからんからごり押し。
+	def fetch (s:Short) = {
+		val strTemp = "%04x".format(s)
+		val str = 
+			if (strTemp.length() != 4) {
+				strTemp.replace("ffff", "")
+			} else {
+				strTemp
+			}
 
-			val str2 = str(2).toString + str(3).toString + str(0).toString + str(1).toString
-			Long.parseLong(str2, 16).toShort
-		}
+		val str2 = str(2).toString + str(3).toString + str(0).toString + str(1).toString
+		Long.parseLong(str2, 16).toShort
+	}
 
+	def makeFile (name:String, list:List[Short]) {
 		// ファイル書き込みオブジェクトを作成
 		val outputFile = new File(name)
 		val fos = new FileOutputStream(outputFile)
 		val dos = new DataOutputStream(fos)
 
-		val list =
-		(for (i <- (0 to listLength.toInt-1)) yield {
-			val n = i.toDouble
-			val value = function(A, f0, samp_freq, n)
-			dos.writeShort(fetch (value.toShort))
-		}).toList
+		list.foreach((value) => {
+			dos.writeShort(fetch (value))
+		})
 
 		dos.close()
+	} 
+
+	def makeSound(function:(Double,Double,Double,Double) => Double):List[Short] = { 
+		(for (i <- (0 to listLength.toInt-1)) yield {
+			val n = i.toDouble
+			function(A, f0, samp_freq, n).toShort
+		}).toList
 	}
-
-
 
 }
